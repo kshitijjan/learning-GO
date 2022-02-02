001ee64eadcadaee562ecc1fcc6827a3bf13703d
Kshitij jain <kshitijjain@Kshitijs-MacBook-Air.local>
2022-02-02 18:18:20 +0530

    Variables and Constants in GO

---
+++ variable-constants/var-const(way 1).go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	const greet = "Hello World!"
+	var name = "Kshitij Jain"
+	fmt.Printf("%v, My name is %v\n", greet, name)
+}
---
+++ variable-constants/var-const(way 2).go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	greet := "Hello World!"
+	name := "Kshitij Jain"
+	fmt.Printf("%v, My name is %v\n", greet, name)
+}
